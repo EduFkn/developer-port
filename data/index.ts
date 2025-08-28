efce7527e240dcb3bc21e82a0972ae80d26ebc0a
EduFkn <ploutzin129@gmail.com>
2025-08-27 21:50:56 -0300

    chore: images and dinamic content index on all sections

---
+++ data/index.ts
@@ -0,0 +1,216 @@
+export const navItems = [
+  { name: "Sobre", link: "#about" },
+  { name: "Projetos", link: "#projects" },
+  { name: "Depoimentos", link: "#testimonials" },
+  { name: "Contato", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Dou prioridade à colaboração com o cliente, promovendo comunicação aberta",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Sou muito flexível com comunicação entre fusos horários",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Minha stack de tecnologias",
+    description: "Estou sempre buscando melhorar",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiasta de tecnologia com paixão por desenvolvimento.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Atualmente desenvolvendo uma biblioteca de animações JS",
+    description: "Por dentro do projeto",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Quer começar um projeto comigo?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Sistema Solar 3D para Explorar",
+    des: "Explore as maravilhas do nosso sistema solar com esta simulação 3D envolvente dos planetas usando Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - App de Videoconferência",
+    des: "Simplifique sua experiência de videoconferência com o Yoom. Conecte-se facilmente com colegas e amigos.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "SaaS de Imagens com IA - Estilo Canva",
+    des: "Um verdadeiro aplicativo SaaS com recursos de IA, sistema de pagamentos e créditos usando tecnologias modernas.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Site 3D Animado do iPhone da Apple",
+    des: "Recriação do site do iPhone 15 Pro da Apple, combinando animações GSAP e efeitos 3D com Three.js.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Colaborar com Adrian foi um verdadeiro prazer. Seu profissionalismo, agilidade e dedicação em entregar resultados excepcionais foram evidentes durante todo o projeto. O entusiasmo de Adrian por cada aspecto do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Adrian é o parceiro ideal.",
+    name: "Michael Johnson",
+    title: "Diretor da AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Colaborar com Adrian foi um verdadeiro prazer. Seu profissionalismo, agilidade e dedicação em entregar resultados excepcionais foram evidentes durante todo o projeto. O entusiasmo de Adrian por cada aspecto do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Adrian é o parceiro ideal.",
+    name: "Michael Johnson",
+    title: "Diretor da AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Colaborar com Adrian foi um verdadeiro prazer. Seu profissionalismo, agilidade e dedicação em entregar resultados excepcionais foram evidentes durante todo o projeto. O entusiasmo de Adrian por cada aspecto do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Adrian é o parceiro ideal.",
+    name: "Michael Johnson",
+    title: "Diretor da AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Colaborar com Adrian foi um verdadeiro prazer. Seu profissionalismo, agilidade e dedicação em entregar resultados excepcionais foram evidentes durante todo o projeto. O entusiasmo de Adrian por cada aspecto do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Adrian é o parceiro ideal.",
+    name: "Michael Johnson",
+    title: "Diretor da AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Colaborar com Adrian foi um verdadeiro prazer. Seu profissionalismo, agilidade e dedicação em entregar resultados excepcionais foram evidentes durante todo o projeto. O entusiasmo de Adrian por cada aspecto do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Adrian é o parceiro ideal.",
+    name: "Michael Johnson",
+    title: "Diretor da AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Estagiário de Engenharia Frontend",
+    desc: "Auxiliou no desenvolvimento de uma plataforma web usando React.js, aprimorando a interatividade.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Dev de App Mobile - JSM Tech",
+    desc: "Projetou e desenvolveu aplicativo móvel para plataformas iOS e Android usando React Native.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Projeto Freelancer de App",
+    desc: "Liderou o desenvolvimento de um app móvel para cliente, desde o conceito até a publicação nas lojas.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Desenvolvedor Frontend Líder",
+    desc: "Desenvolveu e manteve recursos voltados ao usuário usando tecnologias modernas de frontend.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
